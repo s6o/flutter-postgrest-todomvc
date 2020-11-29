f0b11bf7ff495bf158722db64aaf5da6a60a9631
s6o <oliver.s6ro@gmail.com>
2020-11-29 13:57:49 +0200

    Test out customized user JWT specific access to todos table.

---
+++ server/migrations/20201129100349_rpc_todos.sql
@@ -0,0 +1,22 @@
+-- migrate:up
+
+-- Customized user specific todo list
+-- the parameter is the response from /rpc/login aka (app_auth.login)
+DROP FUNCTION IF EXISTS app_api.user_todos;
+CREATE FUNCTION app_api.user_todos(user_login json)
+RETURNS table(id INTEGER, task TEXT, due TIMESTAMPTZ, done BOOLEAN) LANGUAGE sql AS $$
+SELECT
+  t.id
+, t.task
+, t.due
+, t.done
+FROM (
+  SELECT * FROM app_auth.verify(user_login->>'token', current_setting('app.jwt_secret'))
+) AS verified
+INNER JOIN app_api.todos t ON (verified.payload->>'uid')::int = t.user_id
+  AND verified.valid = true;
+$$ ;
+
+-- migrate:down
+
+DROP FUNCTION IF EXISTS app_api.user_todos;
