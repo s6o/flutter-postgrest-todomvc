31fc0c941d070efb200801fe1b914ea7463c8395
s6o <oliver.s6ro@gmail.com>
2020-11-29 13:53:40 +0200

    Update database seed to include some todos table entries for seeded users.

--- server/seed-development.sql
+++ server/seed-development.sql
@@ -3,4 +3,12 @@ INSERT INTO app_auth.users
 VALUES
   ('john.doe@lost.net', 'johnny88', 'John', 'Doe')
 , ('jane.doe@lost.net', 'jenny678', 'Jane', 'Doe')
+ON CONFLICT DO NOTHING
+;
+
+INSERT INTO app_api.todos (user_id, task) VALUES
+  (1, 'Start training tomorrow')
+, (1, 'Buy some milk')
+, (2, 'Start training tomorrow')
+ON CONFLICT DO NOTHING
 ;
