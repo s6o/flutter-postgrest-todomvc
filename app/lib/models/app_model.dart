5a745638d82773c4c3303c21f4c9b36605573e09
Oliver Sõro <oliver.s6ro@gmail.com>
2020-12-09 23:59:37 +0200

    Add todo filtering: all, active, completed; to AppModel.

--- app/lib/models/app_model.dart
+++ app/lib/models/app_model.dart
@@ -2,21 +2,37 @@ import 'package:flutter/material.dart';
 import 'package:todomvc/models/auth.dart';
 import 'package:todomvc/models/todo.dart';
 
+enum TodoFilter {
+  All,
+  Active,
+  Completed,
+}
+
 class AppModel extends ChangeNotifier {
   Credentials _credentials;
   Jwt _jwt;
-  List<Todo> todos;
+  List<Todo> _todos;
+  Map<TodoFilter, bool> _filters;
 
   AppModel()
       : _credentials = Credentials(),
         _jwt = null,
-        todos = [];
+        _filters = {
+          TodoFilter.All: true,
+          TodoFilter.Active: false,
+          TodoFilter.Completed: false,
+        },
+        _todos = [];
 
   Credentials get credentials {
     return _credentials;
   }
 
-  get jwt {
+  bool hasFilter(TodoFilter tf) {
+    return _filters[tf];
+  }
+
+  Jwt get jwt {
     return _jwt;
   }
 
@@ -30,8 +46,30 @@ class AppModel extends ChangeNotifier {
     return _jwt != null && _jwt.token.isNotEmpty;
   }
 
+  void setFilter(TodoFilter tf) {
+    _filters.map((key, value) => MapEntry(key, false)).update(tf, (_) => true);
+  }
+
+  List<Todo> get todos {
+    TodoFilter filter = _filters.keys
+        .fold(TodoFilter.All, (f, TodoFilter tf) => _filters[tf] ? tf : f);
+    switch (filter) {
+      case TodoFilter.Active:
+        return _todos.where((Todo t) => t.done == false).toList();
+      case TodoFilter.Completed:
+        return _todos.where((Todo t) => t.done == true).toList();
+      case TodoFilter.All:
+      default:
+        return _todos;
+    }
+  }
+
+  set todos(List<Todo> todos) {
+    _todos = todos;
+  }
+
   void toggle(int index) {
-    todos[index].done = !todos[index].done;
+    _todos[index].done = !_todos[index].done;
     notifyListeners();
   }
 
