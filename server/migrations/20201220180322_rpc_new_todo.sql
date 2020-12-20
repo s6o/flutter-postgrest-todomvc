db42a4019738b4c22833cd96d0b7e31114616ff2
Oliver Sõro <oliver.s6ro@gmail.com>
2020-12-20 20:48:10 +0200

    Add POST /rpc/user_new_todo; new todo entry for active session user.

---
+++ server/migrations/20201220180322_rpc_new_todo.sql
@@ -0,0 +1,26 @@
+-- migrate:up
+
+-- New user specific TODO entry
+-- the parameter is the response from /rpc/login aka (app_auth.login) with
+-- additional fields:
+-- {token: <string>, task: <string>, due: <string>}
+DROP FUNCTION IF EXISTS app_api.user_new_todo;
+
+CREATE FUNCTION app_api.user_new_todo(user_todo json)
+RETURNS table(id INTEGER, task TEXT, due TIMESTAMPTZ, done BOOLEAN) LANGUAGE sql AS $$
+INSERT INTO app_api.todos (user_id, task, due)
+  SELECT
+    (verified.payload->>'uid')::int
+  , (user_todo->>'task')::text
+  , coalesce((user_todo->>'due')::timestamp, current_timestamp + '1 day'::interval)
+  FROM (
+    SELECT * FROM app_auth.verify(user_todo->>'token', current_setting('app.jwt_secret'))
+  ) AS verified
+  WHERE verified.valid = true
+  RETURNING id, task, due, done;
+$$ ;
+
+
+-- migrate:down
+
+DROP FUNCTION IF EXISTS app_api.user_new_todo;
