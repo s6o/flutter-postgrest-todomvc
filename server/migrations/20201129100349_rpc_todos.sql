909d95996906cc809787d17b1419f1e09be0641d
Oliver Sõro <oliver.s6ro@gmail.com>
2020-12-21 17:02:37 +0200

    Return timestamps from /rpc/user_todos and /rpc/user_new_todo as YYYY-MM-DD HH24:MI:SS.msZ
    This will allow the timestamps to be correctly parsed into app's
    timezone.

--- server/migrations/20201129100349_rpc_todos.sql
+++ server/migrations/20201129100349_rpc_todos.sql
@@ -4,13 +4,13 @@
 -- the parameter is the response from /rpc/login aka (app_auth.login)
 DROP FUNCTION IF EXISTS app_api.user_todos;
 CREATE FUNCTION app_api.user_todos(user_login json)
-RETURNS table(id INTEGER, task TEXT, due TIMESTAMP, done BOOLEAN, done_at TIMESTAMP) LANGUAGE sql AS $$
+RETURNS table(id INTEGER, task TEXT, due TEXT, done BOOLEAN, done_at TEXT) LANGUAGE sql AS $$
 SELECT
   t.id
 , t.task
-, t.due
+, to_char(t.due, 'YYYY-MM-DD HH24:MI:SS.msZ') as due
 , t.done
-, t.done_at
+, to_char(t.done_at, 'YYYY-MM-DD HH24:MI:SS.msZ') as due_at
 FROM (
   SELECT * FROM app_auth.verify(user_login->>'token', current_setting('app.jwt_secret'))
 ) AS verified
